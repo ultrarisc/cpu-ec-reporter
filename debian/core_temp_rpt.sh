e6ef0ad84660977b2e21eb2c0b306931919cb82e
chenfei <chenfei@ultrarisc.com>
2026-01-07 11:13:04 +0800

    Utilize systemd services to implement functions - Send core temperature and sleep information to EC - Control the LED lights of the RTL8211F network card
    Signed-off-by: chenfei <chenfei@ultrarisc.com>

---
+++ debian/core_temp_rpt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+UART_PORT=1
+Msg_Type=04
+Msg_len=01
+Chip_temp=`sensors -j corepvt_ultrarisc-isa-0000|jq '."corepvt_ultrarisc-isa-0000"."Core temp0"."temp1_input"'|cut -d'.' -f1`
+temp_16=`printf "%x\n" $Chip_temp`
+Temp_flag=$?
+if [ "$Temp_flag" -eq 0 ] && [ -c /dev/tty$UART_PORT ];then
+	echo "$Msg_Type$Msg_len$temp_16"|xxd -r -p  >/dev/ttyS$UART_PORT
+fi
